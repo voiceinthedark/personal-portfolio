ceb5feb850e0308767064c27f6c67553d5a11b6e
voiceinthedark <darkrisingforce@gmail.com>
2023-07-24 04:49:20 +0300

    Added Image View modal

--- src/components/projects/ProjectContent.vue
+++ src/components/projects/ProjectContent.vue
@@ -1,11 +1,17 @@
 <template>
     <div>{{ content.description }}</div>
-    <img class="object-cover rounded-md" :src="content.image" alt="project image">
+    <img class="object-cover rounded-md cursor-pointer" :src="content.image" alt="project image" @click="showModal = true">
+    <ImageModal :showModal="showModal" :imageUrl="content.image" @close="showModal = false"/>
 </template>
 
 <script setup>
+import ImageModal from '@/components/shared/ImageModal.vue';
+import { ref } from 'vue';
+
+let showModal = ref(false);
+
 const content = defineProps({
     description: String,    
-    image: String,
+    image: String,    
 })
 </script>
---
+++ src/components/shared/ImageModal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="modal" v-if="showModal">
+    <div class="modal-content">
+      <span class="absolute top-0 left-0 text-white transition-transform cursor-pointer text-7xl hover:rotate-360" @click="$emit('close')">&times;</span>
+      <img :src="imageUrl" alt="Modal Image" @click="$emit('close')"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+    imageUrl: {
+      type: String,
+      required: true
+    },
+    showModal: {
+      type: Boolean,
+      required: true
+    }
+});
+
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  max-width: 80%;
+  max-height: 80%;
+}
+
+img {
+  max-width: 100%;
+  max-height: 100%;
+}
+</style>
