c77d5b09ff86a78a613d9b7517be1655ab106448
voiceinthedark <darkrisingforce@gmail.com>
2023-07-24 04:10:57 +0300

    Added scroll snapping on projects page

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@ console.log(route.name);
 </script>
 
 <template>
-  <header>
+  <header class="mb-16">
     <div class="flex flex-row">
-      <nav class="flex flex-row gap-3 p-3 mb-4 space-x-2 text-2xl bg-transparent">
+      <nav class="fixed flex flex-row w-full gap-3 p-3 space-x-2 text-2xl rounded-sm bg-gradient-to-br from-emerald-200/25 via-cyan-400/25 to-emerald-600/25">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
         <NavLink to="/projects">Projects</NavLink>
@@ -17,7 +17,7 @@ console.log(route.name);
     </div>
   </header>
 
-  <RouterView />
+  <RouterView  />
 </template>
 
 
--- src/components/projects/ProjectList.vue
+++ src/components/projects/ProjectList.vue
@@ -1,15 +1,48 @@
 <script setup>
+import { onMounted } from 'vue';
 import { useProjectStore } from '../../stores/projectStore';
 import ProjectItem from './ProjectItem.vue';
 
 const projects = useProjectStore();
 projects.fetchProjects();
+
+onMounted(() => {
+    const scrollWrapper = document.querySelector(".scroll-wrapper");
+    scrollWrapper.addEventListener("scroll", handleScroll);
+})
+
+function handleScroll(event) {
+    const scrollWrapper = event.target;
+      const scrollPosition = scrollWrapper.scrollTop;
+      const snapHeight = scrollWrapper.clientHeight;
+      const currentIndex = Math.round(scrollPosition / snapHeight);
+      scrollWrapper.scrollTop = currentIndex * snapHeight; 
+}
+
 </script>
 
 <template>
-    <template v-for="project in projects.projects.items" :key="project.id">
-        <div class="mb-2 bg-white/50 w-[80%] min-h-[200px] p-2 rounded-lg shadow-sm">
-            <ProjectItem :project="project" />
+    <div class="scroll-container">
+        <div class="grid grid-flow-row place-items-center">
+            <template v-for="project in projects.projects.items" :key="project.id">
+                <div class="mb-10 bg-white/50 w-[80%] min-h-[200px] p-2 rounded-lg shadow-sm scroll-wrapper">
+                    <ProjectItem :project="project" />
+                </div>
+            </template>
         </div>
-    </template>    
+    </div> 
 </template>
+
+<style>
+.scroll-container {
+  overflow-y: scroll;
+  overscroll-behavior-y: contain;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  height: 100vh;
+}
+
+.scroll-wrapper {
+  scroll-snap-align: start;
+}
+</style>
